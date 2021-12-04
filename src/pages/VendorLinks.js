f85264cbad4fbd24682faa0fda7b459f5ee0312a
Mandy He <mandy_he@brown.edu>
2021-12-04 17:55:38 -0500

    added all links to vendorlinks page

--- src/pages/VendorLinks.js
+++ src/pages/VendorLinks.js
@@ -3,6 +3,51 @@ import React from 'react';
 function VendorLinks() {
     return (
         <div>
+            <h2>United States</h2>
+            <ul>
+                <li><a href="https://kineticlabs.store/" target="_blank" rel="noopener">Kinetic Cables</a></li>
+                <li><a href="https://novelkeys.xyz/" target="_blank" rel="noopener">NovelKeys</a></li>
+                <li><a href="https://thekey.company/" target="_blank" rel="noopener">The Key Dot Company</a></li>
+                <li><a href="https://mechanicalkeyboards.com/" target="_blank" rel="noopener">mechanicalkeyboards.com</a></li>
+                <li><a href="https://cannonkeys.com/" target="_blank" rel="noopener">Cannon Keys</a></li>
+                <li><a href="https://www.primekb.com/" target="_blank" rel="noopener">PrimeKB</a></li>
+                <li><a href="https://keeb.io/" target="_blank" rel="noopener">Keeb.io</a></li>
+                <li><a href="https://www.1upkeyboards.com/" target="_blank" rel="noopener">1upKeyboards</a></li>
+            </ul>	
+
+
+            <h2>Europe</h2>
+            <ul>
+                <li><a href="https://candykeys.com/v" target="_blank" rel="noopener">CandyKeys</a></li>
+                <li><a href="https://splitkb.com/" target="_blank" rel="noopener">SplitKB</a></li>
+                <li><a href="http://www.mechsupply.co.uk/" target="_blank" rel="noopener">MechSupply</a></li>
+                <li><a href="https://keygem.store/" target="_blank" rel="noopener">Keygem</a></li>
+                <li><a href="https://salvun.com/" target="_blank" rel="noopener">Salvun</a></li>
+            </ul>	
+
+            <h2>Canada</h2>	
+            <ul>
+                <li><a href="https://zealpc.net/" target="_blank" rel="noopener">Zeal</a></li>
+                <li><a href="https://www.deskhero.ca/">DeskHero</a></li>
+                <li><a href="https://www.ashkeebs.com/?aff=3" target="_blank" rel="noopener">Ash Keebs</a></li>
+                <li><a href="https://keyspresso.ca/?aff=2" target="_blank" rel="noopener">Keyspresso</a> </li>
+            </ul>
+
+            <h2>Asia</h2>
+            <ul>
+                <li><a href="https://kbdfans.com/?utm_source=simple-affiliate&amp;utm_medium=referral&amp;utm_campaign=Alexander+%281433711247408%29&amp;sacode=meguep" target="_blank" rel="noopener">KBDfans</a></li>
+                <li><a href="https://graystudio.club/" target="_blank" rel="noopener">GrayStudio</a></li>
+                <li><a href="https://kprepublic.com/" target="_blank" rel="noopener">KPRepublic</a></li>
+                <li><a href="https://kibou.store/?ref=dux_nobbmul" target="_blank" rel="noopener">Kibou</a></li>
+                <li><a href="https://monokei.co/" target="_blank" rel="noopener">Monokei</a></li>
+            </ul>
+
+            <h2>Australia</h2>		
+            <ul>
+                <li><a href="https://rama.works/" target="_blank" rel="noopener">Rama Works</a></li>
+                <li><a href="https://www.switchkeys.com.au/" target="_blank" rel="noopener">SwitchKeys</a></li>
+                <li><a href="https://dailyclack.com/collections/stabilisers/products/durock-v2-screw-in-stabilisers" target="_blank" rel="noopener">Daily Clack</a></li>
+            </ul>	
         </div>
     )
 }
