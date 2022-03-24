1ce7efb81fed9147ed04afff252ee53553d74180
Mandy He <mandy_he@brown.edu>
2022-03-24 19:57:17 -0400

    part two of homepage flexbox works

--- src/components/HomeSub2.js
+++ src/components/HomeSub2.js
@@ -1,8 +1,33 @@
 import React from "react";
+import switches from "../photos/home-page/switches.png";
 import "./HomeSub2.css";
 
 function HomeSub2() {
-  return <div className="multicolor"> hi</div>
+  return (
+    <div className="multicolor">
+      <div>
+        <div class="grid-container-2">
+          <div class="item1">
+            <img src={switches} alt="switches"></img>
+          </div>
+          <div class="item2">
+            <img src={switches} alt="icon"></img>
+          </div>
+        </div>
+        <div class="grid-container-3">
+          <div class="item1">
+            1{/* <img src={switches} alt="icon"></img> */}
+          </div>
+          <div class="item2">
+            2{/* <img src={switches} alt="icon"></img> */}
+          </div>
+          <div class="item3">
+            3{/* <img src={switches} alt="icon"></img> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomeSub2;
