ce5e903232f7abc60e719544fa8299ff2a700f41
Mandy He <mandy_he@brown.edu>
2022-03-24 20:49:01 -0400

    added logos to part 2 of home:

--- src/components/HomeSub2.js
+++ src/components/HomeSub2.js
@@ -2,12 +2,36 @@ import React from "react";
 import switches from "../photos/home-page/switches.png";
 import keycaps from "../photos/home-page/Keycaps.png";
 import stabilizers from "../photos/home-page/stabilizers.png";
+import AliExpress from "../photos/home-page/Logos/AliExpress.svg";
+import Amazon from "../photos/home-page/Logos/Amazon.svg";
+import kbdfans from "../photos/home-page/Logos/kbdfans.svg";
+import NovelKeys from "../photos/home-page/Logos/NovelKeys.svg";
+import bang from "../photos/home-page/Logos/bang.svg";
+
 import "./HomeSub2.css";
 
 function HomeSub2() {
   return (
     <div className="multicolor">
+      <p>Browse popular vendors like</p>
       <div>
+        <div class="grid-container-5">
+          <div class="logo">
+            <img src={Amazon} alt="switches"></img>
+          </div>
+          <div class="logo">
+            <img src={kbdfans} alt="icon"></img>
+          </div>
+          <div class="logo">
+            <img src={NovelKeys} alt="switches"></img>
+          </div>
+          <div class="logo">
+            <img src={AliExpress} alt="icon"></img>
+          </div>
+          <div class="logo">
+            <img src={bang} alt="switches"></img>
+          </div>
+        </div>
         <div class="grid-container-2">
           <div class="item1">
             <img src={switches} alt="switches"></img>
